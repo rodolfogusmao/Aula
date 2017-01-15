6da6b0f4024856a810a1afff8f87d64a9c24caea
rodol <rodol@DESKTOP-M91Q89R>
2017-01-15 21:27:59 -0200

    Commit inicial

---
+++ Aulas/src/Calculadora.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		int num1, num2, oper, resultado;
+		System.out.println(">>> Calculadora <<<");
+		System.out.println("Informe o primeiro número: ");
+		num1 = scann.nextInt();
+	}
+
+}
