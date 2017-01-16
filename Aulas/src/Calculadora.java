e623ab9b9c359a260005b766742fd5d552234491
rodol <rodol@DESKTOP-M91Q89R>
2017-01-15 22:28:18 -0200

    Criado switch case e testado

--- Aulas/src/Calculadora.java
+++ Aulas/src/Calculadora.java
@@ -6,12 +6,31 @@ public class Calculadora {
 		Scanner scann = new Scanner(System.in);
 		int num1, num2, oper, resultado;
 		System.out.println(">>> Calculadora <<<");
-		System.out.println("Informe o primeiro número: ");
+		System.out.print("Informe o primeiro número: ");
 		num1 = scann.nextInt();
 		System.out.print("Informe o segundo numero: ");
 		num2 = scann.nextInt();
 		System.out.println("Escolha a operação 1 = soma, 2 = subtração, 3 = divisão ou 4 = multiplicação");
 		oper = scann.nextInt();
+		switch (oper){
+		case 1:
+			resultado = num1+num2;
+			System.out.println("O resultado é " + resultado);
+			break;
+		case 2:
+			resultado = num1-num2;
+			System.out.println("O resultado é "+ resultado);
+			break;
+		case 3:
+			resultado = num1/num2;
+			System.out.println("O resultado é "+ resultado);
+			break;
+		case 4:
+			resultado = num1*num2;
+			System.out.println("O resultado é "+ resultado);
+			break;
+		default:
+			System.out.println("Opção inválida!");
+		}
 	}
-
 }
